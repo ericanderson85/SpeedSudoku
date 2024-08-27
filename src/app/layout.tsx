c9d8a5f514b6805db74afd3e26ea0d5fd11572fb
ericanderson85 <ericandersonnn@icloud.com>
2024-08-27 14:04:04 -0400

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <link rel="icon" sizes="any" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/images/icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/icon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/icon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/icon-180x180.png" />
       <UserProvider>
         <body className={inter.className}>
           <Header />
