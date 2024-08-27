210ec9e5213da3fb26b29d2ec43ad1ea2ef27f60
ericanderson85 <ericandersonnn@icloud.com>
2024-08-27 10:53:41 -0400

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 
 
 export default async function Home() {
-  // const user = await getUserData();
+  const user = await getUserData();
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <Link href="/play" className="text-4xl font-semibold mt-4">Play</Link>
       <Link href="/leaderboard" className="text-4xl font-semibold">Leaderboard</Link>
-      {/* <div className="mb-12">
+      <div className="mb-12">
         {
           user ?
             <Link href="/me" className="text-4xl font-semibold">Profile</Link>
             :
             <Link href="/api/auth/login" className="text-4xl font-semibold">Log In / Sign Up</Link>
         }
-      </div> */}
+      </div>
       <div className="flex items-center justify-center flex-col">
         <h1 className="text-2xl font-medium">How it works:</h1>
         <p>You get 5 minutes to finish a sudoku puzzle with a difficulty matched to your Elo score.</p>
